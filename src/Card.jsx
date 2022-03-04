8de4fa1e01cce7a8902ced9f2c5bc3d49e3d8203
Sarath <sarath.s.m@accenture.com>
2022-03-04 16:56:29 +0530

    cards added in grid layout

---
+++ src/Card.jsx
@@ -0,0 +1,12 @@
+import React, { Fragment } from "react";
+import './card.css';
+
+const Card = () => {
+  return(<Fragment>
+    <div className="card-container">
+      This is card
+    </div>
+  </Fragment>)
+};
+
+export default Card;
